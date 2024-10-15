06bbcd1084631a3c0e1ffec4d837d94d44e84165
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-15 15:02:00 +0300

    feat: create TablesInfo array, include js in html

---
+++ scripts/consts.js
@@ -0,0 +1,44 @@
+const TablesInfo = [
+  {
+    title: 'Spring Boot',
+    imageSrc: './assets/spring-boot.svg',
+    description:
+      'Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible.',
+    cardUrl: 'https://spring.io/projects/spring-boot',
+  },
+  {
+    title: 'Spring Framework',
+    imageSrc: './assets/spring-framework.svg',
+    description:
+      'Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.',
+    cardUrl: 'https://spring.io/projects/spring-framework',
+  },
+  {
+    title: 'Spring Data',
+    imageSrc: './assets/spring-data.svg',
+    description:
+      'Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.',
+    cardUrl: 'https://spring.io/projects/spring-data',
+  },
+  {
+    title: 'Spring Cloud',
+    imageSrc: './assets/spring-cloud.svg',
+    description:
+      'Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.',
+    cardUrl: 'https://spring.io/projects/spring-cloud',
+  },
+  {
+    title: 'Spring Cloud Data Flow',
+    imageSrc: './assets/spring-data-flow.svg',
+    description:
+      'Provides an orchestration service for composable data microservice applications on modern runtimes.',
+    cardUrl: 'https://spring.io/projects/spring-data-flow',
+  },
+  {
+    title: 'Spring Security',
+    imageSrc: './assets/spring-security.svg',
+    description:
+      'Protects your application with comprehensive and extensible authentication and authorization support.',
+    cardUrl: 'https://spring.io/projects/spring-security',
+  },
+];
