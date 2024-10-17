0b176ef22a9cf9964052f6c3ec4d151be20c95b8
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-17 16:25:59 +0300

    feat: implement card searching by title

--- scripts/script.js
+++ scripts/script.js
@@ -87,5 +87,42 @@ navItemsName.forEach((el) => {
     if (!isOpen) {
       el.classList.add('open');
     }
-  })
+  });
 });
+
+// Search input
+const searchInput = document.querySelector('.search-input');
+
+const searchData = (event) => {
+  cardsContainer.innerHTML = '';
+  const value = event.target.value.toLowerCase();
+  const correctValues = TablesInfo.filter((item) => item.title.toLowerCase().includes(value));
+  correctValues.forEach((info) => {
+    const card = document.createElement('a');
+    card.href = info.cardUrl;
+    card.className = 'card-link';
+    card.innerHTML = `<div class="card">
+                <div class="card__image-wrapper">
+                  <img class="card__image" src=${info.imageSrc} alt=${info.title} />
+                </div>
+                <div class="card__info">
+                  <h3 class="card__info-title">${info.title}</h3>
+                  <p class="card__info-description">${info.description}</p>
+                </div>
+              </div>`;
+    cardsContainer.append(card);
+  });
+};
+
+const debounce = (callback, waitTime) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, waitTime);
+  };
+};
+
+const debounceHandler = debounce(searchData, 300);
+searchInput.addEventListener('keyup', debounceHandler);
